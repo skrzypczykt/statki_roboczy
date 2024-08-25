8c072ac7371c84c771c581f5a71df9012d91b014
tomaszs <skrzypczykt@gmail.com>
2024-08-25 19:34:50 +0200

    ResNet50V2

--- train_model.py
+++ train_model.py
@@ -1,9 +1,15 @@
 from src.data import get_data_generators
 import yaml
+import tensorflow as tf
+from tensorflow.keras import mixed_precision
 
 from src.model_trainer import train_model
 
 if __name__ == "__main__":
+    tf.config.optimizer.set_jit(True)
+    policy = mixed_precision.Policy('mixed_float16')
+    mixed_precision.set_global_policy(policy)
+
     # Parameters
     with open("config.yaml", "r") as fp:
         params = yaml.safe_load(fp)
